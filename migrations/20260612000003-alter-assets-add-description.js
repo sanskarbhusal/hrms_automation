221d9bb7a034ffd370a4dcb0ca7f87c1d063e37a
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-06-24 17:05:24 +0545

    feat: remove migration and seeder files Not needed that's why

--- migrations/20260612000003-alter-assets-add-description.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(queryInterface) {
-    await queryInterface.sequelize.query('ALTER TABLE assets ADD COLUMN description TEXT NULL AFTER `name`');
-  },
-
-  async down(queryInterface) {
-    await queryInterface.sequelize.query('ALTER TABLE assets DROP COLUMN description');
-  },
-};
