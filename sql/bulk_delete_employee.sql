4f9c3746ff7f30104cdc25de6b3ef93238e7a26f
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-07-04 10:02:23 +0545

    feat: remove hardcoded credentials from login test

---
+++ sql/bulk_delete_employee.sql
@@ -0,0 +1,8 @@
+truncate table employee_lifecycle_events;
+truncate table attendance_regularizations;
+delete from attendance_records;
+delete from employee_documents;
+delete from leave_balances;
+delete from leave_requests;
+delete from time_select_requests;
+delete from employees;
